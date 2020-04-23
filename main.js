f57b83a97487c51b44ae4673a9e440ef2d2db8b8
amin roosta <amin.roosta@outlook.com>
2020-04-23 15:55:25 +0800

    make crosshair date farsi

--- main.js
+++ main.js
@@ -6,7 +6,31 @@ window.tvWidget = new TradingView.widget({
     interval: '1D', // default interval
     fullscreen: true, // displays the chart in the fullscreen mode
     container_id: 'tv_chart_container',
+    timezone: 'Asia/Tehran',
     datafeed: Datafeed,
     locale: 'fa',
     library_path: '../charting_library/',
+    customFormatters: {
+        timeFormatter: {
+            format: (e) => {
+                return e.toLocaleTimeString('fa-IR')
+                    + '  ' + e.toLocaleDateString('fa-IR', {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'numeric',
+                        day: 'numeric'
+                    });
+            }
+        },
+        dateFormatter: {
+            format: (e) => {
+                return e.toLocaleDateString('fa-IR', {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'numeric',
+                        day: 'numeric'
+                    });
+            }
+        }
+    },
 });
