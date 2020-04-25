90e270f9f5b39e5f0d4811a1ea522afbb3e9fc08
amin roosta <amin.roosta@outlook.com>
2020-04-25 21:41:10 +0800

    update ip address

--- datafeed.js
+++ datafeed.js
@@ -3,7 +3,7 @@ import jalaali from './jalali.js';
 const lastBarsCache = new Map();
 
 // current public ip address - needs to be made static.
-const ip = '202.185.36.83';
+const ip = '202.185.172.27';
 
 // for more info see https://cors-anywhere.herokuapp.com/http://api.catphotos.io/
 let endpoint = `https://cors-anywhere.herokuapp.com/http://${ip}:80`;
