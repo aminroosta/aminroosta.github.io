5f33cc97e804f33756393305726b697c9d8c50dd
amin roosta <amin.roosta@outlook.com>
2021-01-24 08:10:01 +0000

    Updates

--- mychart/mychart.d.ts
+++ mychart/mychart.d.ts
@@ -1,6 +1,6 @@
 declare module "index" {
-    import { Buffer, DrawConfig } from "regl";
-    export const temp = 5;
+    import regl, { Buffer, DrawConfig } from "regl";
+    export const createREGL: typeof regl;
     export const Triangle: DrawConfig<{
         color: [number, number, number, number];
         resolution: [number, number];
