f33ca931b508a96857465fd233c5f260d34198e7
amin roosta <amin.roosta@outlook.com>
2021-01-26 10:00:11 +0000

    Updates

--- mychart/mychart.d.ts
+++ mychart/mychart.d.ts
@@ -1,3 +1,22 @@
+declare module "data-source" {
+    import { Buffer, Regl } from "regl";
+    type Bar = {
+        epoch: number;
+        open: number;
+        high: number;
+        low: number;
+        close: number;
+        volume: number;
+    };
+    export function buildDataSource(): Bar[] & {
+        buffers: (r: Regl) => {
+            bar2x: Buffer;
+            bar3x: Buffer;
+            bar2x_cpu: Float32Array;
+            bar3x_cpu: Float32Array;
+        };
+    };
+}
 declare module "index" {
     import { Buffer, DrawConfig } from "regl";
     export const temp = 5;
